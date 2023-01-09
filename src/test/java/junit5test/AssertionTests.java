8a205e100230febc92b2d7f8325b36e56d4a7ebe
Kostantin K <110024628+kostantink89@users.noreply.github.com>
2023-01-09 13:55:50 +0200

    JUnit 5 Course

--- src/test/java/junit5test/AssertionTests.java
+++ src/test/java/junit5test/AssertionTests.java
@@ -1,11 +1,15 @@
 package junit5test;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionTests {
@@ -48,6 +52,39 @@ public class AssertionTests {
                 () -> assertEquals("firstString", "secondString", "The String " + "values were not equal"),
                 () -> assertThrows(NullPointerException.class, null),
                 () -> assertTrue(false, "This boolean condition did not evaluate to true"));
+
+    }
+
+    @Test
+    void assertForMapTest() {
+        Map<String, Integer> theMap = new HashMap<>();
+        theMap.put("firstKey", 1);
+        theMap.put("secondKey", 2);
+        theMap.put("thirdKey", 3);
+
+        assertThat(theMap, Matchers.hasValue(2));
+        assertThat(theMap, Matchers.hasKey("secondKey"));
+    }
+
+    @Test
+    void assertForList() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, Matchers.hasItem("thirdString"));
+    }
+
+    @Test
+    void assertForAnyOf() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, Matchers.anyOf(Matchers.hasItem("thirdString"), Matchers.hasItem("noString")));
+    }
+
+    @Test
+    void assertForContainsAnyOrder() {
+        List<String> theList = Arrays.asList("firstString", "secondString", "thirdString");
+
+        assertThat(theList, Matchers.containsInAnyOrder("firstString", "secondString", "thirdString"));
 
     }
 }
